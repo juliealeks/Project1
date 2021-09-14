60a079ed2e047a7333d1740152680344acadd16c
Juliealeks <juliealeks@gmail.com>
2021-09-14 14:03:06 +0300

    new

---
+++ src/com/stepit/practice1/FirstApp.java
@@ -0,0 +1,13 @@
+package com.stepit.practice1;
+
+// Fig. 2.1: First App.java
+// Text-printing program.
+public class FirstApp
+{
+    // main method begins execution of Java application
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Java Programming!");
+    } // end method main
+// end class FirstApp
+}
