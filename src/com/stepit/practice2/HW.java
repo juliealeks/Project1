dca3f28c254319fa619267be0fc9f83e17c7a6e4
juliealeks <88795750+juliealeks@users.noreply.github.com>
2021-09-16 22:15:47 +0300

    Update HW.java

--- src/com/stepit/practice2/HW.java
+++ src/com/stepit/practice2/HW.java
@@ -58,13 +58,13 @@ public class HW {
                 }
                 while (UserNumber != UnknownNumber);
                 System.out.println("Number of attempts: " + TrysCount);
-                int En = 1;
+        
+               int En = 1;
                int Fr = 2;
                int It = 3;
                int Sp = 4;
                int Ru = 5;
                int Un = 6;
-
                int enter;
            do {
             System.out.println("Please choose your language: English -1,French -2, Italian -3, Spanish -4, Russian -5.");
@@ -90,34 +90,4 @@ public class HW {
    // end class HW
                 
 
-               int En = 1;
-               int Fr = 2;
-               int It = 3;
-               int Sp = 4;
-               int Ru = 5;
-               int Un = 6;
-
-               int enter;
-           do {
-            System.out.println("Please choose your language: English -1,French -2, Italian -3, Spanish -4, Russian -5.");
-            enter = input.nextInt();
-            if (enter == 1 )
-                System.out.println("Hello! ". toUpperCase());
-            else if (enter == 2 )
-                System.out.println("Bon jour! ". toUpperCase());
-            else if (enter == 3 )
-                System.out.println("Buon giorno! ". toUpperCase());
-            else if (enter == 4 )
-                System.out.println("Hola! ". toUpperCase());
-            else if (enter == 5)
-                System.out.println("Привет! ". toUpperCase());
-                      }
-            while (enter <= 5);
-               System.out.println("Unknowing language. ");
-
-        }
-    }
-
-           //  end method main
-   // end class HW
-
+               
